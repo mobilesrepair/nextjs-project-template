e8b6f6a222aa2f3683713a71d1d1e13f3b96098b
mobilesrepair <jubikseta@gmail.com>
2025-06-18 04:31:07 +0800

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1750192268163

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_43d6b56b._.js",
+  "static/chunks/src_9c052eca._.js",
+  "static/chunks/src_styles_antivirus_e2d64ecd.css"
+],
+    source: "dynamic"
+});
